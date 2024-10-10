f4398463a357a216de1180fc6d55d2c67ef89a15
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 22:13:25 -0300

    Azure Pipeline02

--- terraform/provider-azure.tf
+++ terraform/provider-azure.tf
@@ -1,15 +1,28 @@
 terraform {
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
+      version = ">= 4.4.0"
     }
   }
-
+  backend "azurerm" {
+    resource_group_name  = "rg-staticsite-lb-tf-fernanda"
+    storage_account_name = "staticsitelbtffernanda"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-  features {}
-  alias = "cloud"
+  resource_provider_registrations = "none"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+    name     = "rg-staticsite-lb-fernanda"
+    location = "brazilsouth"
 }
